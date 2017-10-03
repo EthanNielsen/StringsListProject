a5f0f16cb791b128fb563c02fd5b939cd71b37ec
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-10-03 12:37:02 -0600

    used the /** + Enter to created easy comments.

--- src/stringlist/view/PopupDisplay.java
+++ src/stringlist/view/PopupDisplay.java
@@ -4,11 +4,20 @@ import javax.swing.JOptionPane;
 
 public class PopupDisplay
 {
+	/**
+	 * Displays a popup showing the supplied String to the user.
+	 * @param textToDisplay The text to be displayed.
+	 */
 	public void displayText(String textToDisplay)
 	{
 		JOptionPane.showMessageDialog(null,  textToDisplay);
 	}
 	
+	/**
+	 * Uses a popup to display a question to the user and returns their response as a Stringg.
+	 * @param fromQuestion The question being asked.
+	 * @return The users response to the question.
+	 */
 	public String getResponse(String fromQuestion)
 	{
 		String answer = "";
