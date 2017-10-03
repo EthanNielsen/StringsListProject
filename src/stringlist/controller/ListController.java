4cd782306a1f0fff3eabec16e1d5f9b711539088
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-10-03 13:17:22 -0600

    added multiple lists to the list Conroller, inside the "fillTheList()".

--- src/stringlist/controller/ListController.java
+++ src/stringlist/controller/ListController.java
@@ -1,11 +1,39 @@
 package stringlist.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+import stringlist.model.kahoot;
+
 public class ListController
 {
+	private List<kahoot> myKahoots;
+	
+	public ListController()
+	{
+		myKahoots = new ArrayList<kahoot>();
+	}
 	
 	public void start()
 	{
-		
-	}
+		kahoot myFirstKahoot = new kahoot();
+		myKahoots.add(myFirstKahoot);
+	}	
 	
+	private void fillTheList()
+	{
+		kahoot fiftyStates = new kahoot("Kashish", 50);
+		kahoot mySecondKahoot = new kahoot("Ethan", 2);
+		kahoot bigQuiz = new kahoot("Derek", Integer.MAX_VALUE);
+		kahoot annimalColor = new kahoot("Branton", 10);
+		kahoot presidents = new kahoot("Obama", 44);
+		myKahoots.add(fiftyStates);
+		myKahoots.add(mySecondKahoot);
+		myKahoots.add(bigQuiz);
+		myKahoots.add(presidents);
+		
+//		for (int kahoot = 0; kahoot < 5; kahoot +=1)
+		{
+			kahoot mySecondKahoot = new kahoot("Ethan", 2);
+			myKahoots.add(mySecondKahoot);
+		}
 }
--- src/stringlist/model/kahoot.java
+++ src/stringlist/model/kahoot.java
@@ -2,5 +2,24 @@ package stringlist.model;
 
 public class kahoot
 {
-
+	private int questionCount;
+	private int level;
+	private String creator;
+	private boolean isJumble;
+	
+	
+	
+	public kahoot()
+	{
+		this.questionCount = 0;
+		this.level = 1;
+		this.creator = "nobody";
+		this.isJumble = false;
+	}
+	
+	public kahoot(String creator, int questionount)
+	{
+		this.creator = creator;
+		this.questionCount = questionCount;
+	}
 }
